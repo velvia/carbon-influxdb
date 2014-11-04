9113e687342f24914da1509863698d3e69f5b72b
Evan Chan <velvia@gmail.com>
2014-11-04 14:20:05 -0800

    Add carbon-tee.sh script

---
+++ carbon-tee.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# != 5 ]
+then
+    echo "usage: $0 <src-port> <dest1-host> <dest1-port> <dest2-host> <dest2-port>"
+    exit 0
+fi
+
+TMP=`mktemp -d`
+SOURCE=$TMP/pipe.source
+trap 'rm -rf "$TMP"' EXIT
+mkfifo -m 0600 "$SOURCE"
+echo Made fifo $SOURCE
+nc "$4" "$5" <"$SOURCE" &
+echo Started forking from $SOURCE to $4 $5
+# -k is needed for Carbon/Graphite because connection may be closed after each batch
+nc -k -l "$1" | tee "$SOURCE" | nc "$2" "$3"
